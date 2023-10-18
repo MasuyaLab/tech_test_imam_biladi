c18829316ed97350150137055bcf8ac9003662ea
Roziq Rizal <86276259+masuyamaster@users.noreply.github.com>
2023-10-18 08:29:48 +0700

    commit

---
+++ app/src/androidTest/java/com/masuya/imambiladitechnicaltest/ListUsers.java
@@ -0,0 +1,4 @@
+package com.masuya.imambiladitechnicaltest;
+
+public class ListUsers {
+}
---
+++ app/src/androidTest/java/com/masuya/imambiladitechnicaltest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.masuya.imambiladitechnicaltest;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity{
+public static final String URL="http://masuya1.freeddns.org:91/cobaapp2/masuyates/technicaltest/";
+private boolean isNetworkOk;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    public void runCode(View view){
+    if(isNetworkOk){
+
+    }
+    }
+}
